018031fa691ffb2316cd9ee2b16b8364d7f98423
Kushal Niroula <niroulakushal31@gmail.com>
2023-09-07 10:07:10 +0545

    feat: add unit

---
+++ Models/ProductUnit.cs
@@ -0,0 +1,7 @@
+﻿namespace bca_vi_august.Models;
+
+public class ProductUnit
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ ViewModels/UnitIndexVm.cs
@@ -0,0 +1,12 @@
+﻿using bca_vi_august.Models;
+
+namespace bca_vi_august.ViewModels;
+
+public class UnitIndexVm
+{
+    // For Retrieving user input
+    public string Name { get; set; }
+
+    // Sending data to view
+    public List<ProductUnit> Data;
+}
